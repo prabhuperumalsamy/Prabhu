518faf9dfb1e5e1b3c6310e4dcfb3d0abf80a7c5
prabhuperumalsamy <109942625+prabhuperumalsamy@users.noreply.github.com>
2022-08-30 16:56:56 +0530

    test

--- imagefile.sh
+++ imagefile.sh
@@ -7,7 +7,7 @@ aws configure set aws_access_key_id $accesskey; aws configure set aws_secret_acc
 echo AWS credentials configured Successfully
 
 echo Checking for repo at ECR
-echo tag="3108c477b1a69255679b14cb425b3a8a867e7faf"
+tag=3108c477b1a69255679b14cb425b3a8a867e7faf
 repo=556277294023.dkr.ecr.us-east-1.amazonaws.com/actimize-test-efiler
 sed -i 's@apache:apache@'"$repo:$tag"'@' ./deploy.yaml
 echo $tag :$repo
