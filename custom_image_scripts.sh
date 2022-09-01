da2c8d4bd8e4e151bdf56a47784a888588362c9c
prabhuperumalsamy <109942625+prabhuperumalsamy@users.noreply.github.com>
2022-09-01 20:37:06 +0530

    test

--- custom_image_scripts.sh
+++ custom_image_scripts.sh
@@ -1,12 +1,11 @@
 #The below commands are used to get accesskey and secret key from the aws secret manager
-echo checking for aws credentials from Secret Manager
+echo Checking for AWS credentials from Secret Manager.....
 accesskey=$(aws secretsmanager get-secret-value --secret-id 'efiler_test' --query 'SecretString' --output text | jq -r '.efileraccesskey')
 secretkey=$(aws secretsmanager get-secret-value --secret-id 'efiler_test' --query 'SecretString' --output text | jq -r '.efilersecretkey')
 
 #The received aws credentials are configured inside the custom image 
-echo aws credentials retrieved from secret manager.......
 aws configure set aws_access_key_id $accesskey; aws configure set aws_secret_access_key $secretkey; aws configure set default.region "us-east-1"; aws configure set default.format "json"
-echo AWS credentials configured Successfully
+echo AWS credentials configured inside custom image Successfully
 
 #Fecthing user inputs from manual_deployment_parameters.yaml file and proceeding for deployment
 echo Checking for user inputs from mamaul_deployment_parameters.yaml file
@@ -19,7 +18,7 @@ sed -i 's@apache:apache@'"$repo:$tag"'@' ./$app.yaml
 
 #Command used to find the current image running inside the pod
 oldimage=$(kubectl describe deployment efiler -n actimize | grep Image)
-echo current running $app $oldimage
+echo Current running $app $oldimage
 
 #Command used to display the image details which we are going to deploy
 echo ---------------------------------------------------------------------------------------
@@ -32,8 +31,10 @@ aws eks --region us-east-1 update-kubeconfig --name $cluster
 #command to inititate the deployment
 echo Deployment has been initiated........
 kubectl apply -f $app.yaml -n actimize
+echo ---------------------------------------------------------------------------------------
 
 #command used to check the Pod status post deployment 
+echo Please find below the $app pod status....
 sleep 60
 kubectl get pods -n actimize  | grep $app
 
