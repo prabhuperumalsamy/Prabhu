76bc45eaa3c4060d829a6032bcbe101bae6c07c5
prabhuperumalsamy <109942625+prabhuperumalsamy@users.noreply.github.com>
2022-09-06 18:58:12 +0530

    test

--- custom_image_scripts.sh
+++ custom_image_scripts.sh
@@ -17,10 +17,6 @@ repo=556277294023.dkr.ecr.us-east-1.amazonaws.com/actimize-$env-$app
 
 #checking user inputs with ECR Registry
 ecrtag=$(aws ecr describe-images --repository-name=actimize-$env-$app  --image-ids=imageTag=$tag | jq '.imageDetails[0].imageTags[0]' -r)
-if [[ $? == 0 ]]; then
-    echo $tag not found in ECR
-    exit 1
-else    
 sed -i 's@apache:apache@'"$repo:$ecrtag"'@' ./$app.yaml
 echo The given Image tag found in ECR Repository
 
