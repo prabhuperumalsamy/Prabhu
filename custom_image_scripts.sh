6174272dc5753f35bdfb9aee6cf505ebcea49f26
prabhuperumalsamy <109942625+prabhuperumalsamy@users.noreply.github.com>
2022-09-07 13:43:00 +0530

    test

--- custom_image_scripts.sh
+++ custom_image_scripts.sh
@@ -10,14 +10,14 @@ echo AWS credentials configured inside custom image Successfully
 
 #Fecthing user inputs from manual_deployment_parameters.yaml file and proceeding for deployment
 echo Checking for user inputs from mamaul_deployment_parameters.yaml file
-tag=$(grep -w "deployment_tag" ./manual_deployment_parameters.yaml | awk -F= '{print $2}') ./buildspec.yaml
+tag=$(grep -w "deployment_tag" ./manual_deployment_parameters.yaml | awk -F= '{print $2}')
 env=$(grep -w "environment" ./manual_deployment_parameters.yaml | awk -F= '{print $2}')
 app=$(grep -w "application" ./manual_deployment_parameters.yaml | awk -F= '{print $2}')
 cluster=$(grep -w "cluster" ./manual_deployment_parameters.yaml | awk -F= '{print $2}')
 repo=556277294023.dkr.ecr.us-east-1.amazonaws.com/actimize-$env-$app
 
 #checking user inputs with ECR Registry
-ecrtag=$(aws ecr describe-images --repository-name=actimize-$env-$app  --image-ids=imageTag=$tag | jq '.imageDetails[0].imageTags[0]' -r) ./buildspec.yaml
+ecrtag=$(aws ecr describe-images --repository-name=actimize-$env-$app  --image-ids=imageTag=$tag | jq '.imageDetails[0].imageTags[0]' -r)
 sed -i 's@apache:apache@'"$repo:$ecrtag"'@' ./$app.yaml
 echo The given Image tag found in ECR Repository
 
@@ -36,7 +36,7 @@ aws eks --region us-east-1 update-kubeconfig --name $cluster
 
 #command to inititate the deployment
 echo Deployment has been initiated........
-#kubectl apply -f $app.yaml -n actimize
+kubectl apply -f $app.yaml -n actimize
 echo ---------------------------------------------------------------------------------------
 
 #command used to check the Pod status post deployment 
