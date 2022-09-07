6a5c04e2aa952a50426bd9c7b8a6c7453ed4d592
prabhuperumalsamy <109942625+prabhuperumalsamy@users.noreply.github.com>
2022-09-07 17:15:27 +0530

    test

--- custom_image_scripts.sh
+++ custom_image_scripts.sh
@@ -17,11 +17,10 @@ cluster=$(grep -w "cluster" ./manual_deployment_parameters.yaml | awk -F= '{prin
 repo=556277294023.dkr.ecr.us-east-1.amazonaws.com/actimize-$env-$app
 
 #checking user inputs with ECR Registry
-ecrtag=$(aws ecr describe-images --repository-name=actimize-$env-$app  --image-ids=imageTag=$tag | jq '.imageDetails[0].imageTags[0]' -r)
-echo $ecrtag
-echo $tag
 #!/bin/bash
-if [ "$ecrtag" == "$tag" ]; then
+ecrtag=$(aws ecr describe-images --repository-name=actimize-$env-$app  --image-ids=imageTag=$tag | jq '.imageDetails[0].imageTags[0]' -r)
+tag1=$tag
+if [ "$ecrtag" == "$tag1" ]; then
 sed -i 's@apache:apache@'"$repo:$ecrtag"'@' ./$app.yaml
 echo The given Image tag found in ECR Repository;
 else
